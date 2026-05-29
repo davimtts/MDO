4d2fb4e8d420b300f5df946a7bdda6724ba11166
davimtts <davizimmt@gmail.com>
2026-05-29 12:59:15 -0300

    problemas com rotas

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,7 +1,7 @@
 export const SESSION_KEY = 'iris_user_session';
 
 export const ROUTES = {
-  login: "/login/",
-  inicio: "/inicio/",
-  clientes: "/clientes/"
+  login: "./login/",
+  inicio: "./inicio/",
+  clientes: "./clientes/"
 };
